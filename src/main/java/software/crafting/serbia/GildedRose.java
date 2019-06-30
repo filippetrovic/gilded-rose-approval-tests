5785e498aa28df658da4c7c4fb0fe604449b4f6c
Filip Petrovic <fpetrovic92@gmail.com>
2019-06-30 15:09:21 +0200

    Simplify condition

--- src/main/java/software/crafting/serbia/GildedRose.java
+++ src/main/java/software/crafting/serbia/GildedRose.java
@@ -47,7 +47,7 @@ public class GildedRose {
       item.sellIn = item.sellIn - 1;
 
       if (item.sellIn < 0) {
-        item.quality = item.quality - item.quality;
+        item.quality = 0;
       }
     } else {
       if (item.quality > 0) {
