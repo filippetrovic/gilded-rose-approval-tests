a2ee0ba9eb73038e94665db9481233a71e88b905
Filip Petrovic <fpetrovic92@gmail.com>
2019-06-30 15:30:53 +0200

    Delete unused methods

--- src/main/java/software/crafting/serbia/GildedRose.java
+++ src/main/java/software/crafting/serbia/GildedRose.java
@@ -25,42 +25,4 @@ public class GildedRose {
     }
   }
 
-  private void updateBackstagePasses(Item item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-
-      if (item.sellIn < 11) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-
-      if (item.sellIn < 6) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
-    }
-
-    item.sellIn = item.sellIn - 1;
-
-    if (item.sellIn < 0) {
-      item.quality = 0;
-    }
-  }
-
-  private void updateAgedBrie(Item item) {
-    if (item.quality < 50) {
-      item.quality = item.quality + 1;
-    }
-
-    item.sellIn = item.sellIn - 1;
-
-    if (item.sellIn < 0) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    }
-  }
-
 }
