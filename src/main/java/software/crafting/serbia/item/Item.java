2f9a186afd2509a2730f205b46b649fb464917ee
Filip Petrovic <fpetrovic92@gmail.com>
2019-06-30 15:37:52 +0200

    Extract method

--- src/main/java/software/crafting/serbia/item/Item.java
+++ src/main/java/software/crafting/serbia/item/Item.java
@@ -34,16 +34,18 @@ public class Item {
   }
 
   public void update() {
-    if (quality > 0) {
-      quality = quality - 1;
-    }
+    decrementQuality();
 
     sellIn = sellIn - 1;
 
     if (sellIn < 0) {
-      if (quality > 0) {
-        quality = quality - 1;
-      }
+      decrementQuality();
+    }
+  }
+
+  private void decrementQuality() {
+    if (quality > 0) {
+      quality = quality - 1;
     }
   }
 }
