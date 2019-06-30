ef76b8142c89677aa4ae0bc6d55c55a0e44e1bcd
Filip Petrovic <fpetrovic92@gmail.com>
2019-06-30 15:09:02 +0200

    "Backstage passes" condition extracted

--- src/main/java/software/crafting/serbia/GildedRose.java
+++ src/main/java/software/crafting/serbia/GildedRose.java
@@ -27,23 +27,7 @@ public class GildedRose {
           item.quality = item.quality + 1;
         }
       }
-    } else {
-      if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        tmp(item);
-      } else {
-        tmp(item);
-      }
-    }
-  }
-
-  private void tmp(Item item) {
-    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      if (item.quality > 0) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-          item.quality = item.quality - 1;
-        }
-      }
-    } else {
+    } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
       if (item.quality < 50) {
         item.quality = item.quality + 1;
 
@@ -59,21 +43,29 @@ public class GildedRose {
           }
         }
       }
-    }
 
-    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
       item.sellIn = item.sellIn - 1;
-    }
 
-    if (item.sellIn < 0) {
-      if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (item.sellIn < 0) {
+        item.quality = item.quality - item.quality;
+      }
+    } else {
+      if (item.quality > 0) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+          item.quality = item.quality - 1;
+        }
+      }
+
+      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        item.sellIn = item.sellIn - 1;
+      }
+
+      if (item.sellIn < 0) {
         if (item.quality > 0) {
           if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.quality = item.quality - 1;
           }
         }
-      } else {
-        item.quality = item.quality - item.quality;
       }
     }
   }
