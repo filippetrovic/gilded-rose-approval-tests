d4f8727ed3f0b0cbecf8f27a411dfa87ef5ddea6
Filip Petrovic <fpetrovic92@gmail.com>
2021-05-28 11:24:08 +0200

    Move instance method

--- src/main/java/software/crafting/serbia/GildedRose.java
+++ src/main/java/software/crafting/serbia/GildedRose.java
@@ -10,65 +10,7 @@ public class GildedRose {
 
   public void updateQuality() {
     for (Item item : items) {
-      updateQuality(item);
-    }
-  }
-
-  private void updateQuality(Item item) {
-    switch (item.name) {
-      case "Backstage passes to a TAFKAL80ETC concert":
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-
-          if (item.sellIn < 11) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-
-          if (item.sellIn < 6) {
-            if (item.quality < 50) {
-              item.quality = item.quality + 1;
-            }
-          }
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-          item.quality = item.quality - item.quality;
-        }
-        break;
-      case "Aged Brie":
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-        break;
-      case "Sulfuras, Hand of Ragnaros":
-
-        break;
-      default:
-        if (item.quality > 0) {
-          item.quality = item.quality - 1;
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
-        }
-        break;
+      item.updateQuality();
     }
   }
 
--- src/main/java/software/crafting/serbia/Item.java
+++ src/main/java/software/crafting/serbia/Item.java
@@ -18,4 +18,62 @@ public class Item {
   public String toString() {
     return this.name + ", " + this.sellIn + ", " + this.quality;
   }
+
+  public void updateQuality() {
+    switch (name) {
+      case "Backstage passes to a TAFKAL80ETC concert":
+        if (quality < 50) {
+          quality = quality + 1;
+
+          if (sellIn < 11) {
+            if (quality < 50) {
+              quality = quality + 1;
+            }
+          }
+
+          if (sellIn < 6) {
+            if (quality < 50) {
+              quality = quality + 1;
+            }
+          }
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+          quality = quality - quality;
+        }
+        break;
+      case "Aged Brie":
+        if (quality < 50) {
+          quality = quality + 1;
+
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+          if (quality < 50) {
+            quality = quality + 1;
+          }
+        }
+        break;
+      case "Sulfuras, Hand of Ragnaros":
+
+        break;
+      default:
+        if (quality > 0) {
+          quality = quality - 1;
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+          if (quality > 0) {
+            quality = quality - 1;
+          }
+        }
+        break;
+    }
+  }
 }
