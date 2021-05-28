0a969a744626926f7425d3619d6b9e834cf25c1b
Filip Petrovic <fpetrovic92@gmail.com>
2021-05-28 11:18:51 +0200

    liftup condition

--- src/main/java/software/crafting/serbia/GildedRose.java
+++ src/main/java/software/crafting/serbia/GildedRose.java
@@ -38,33 +38,35 @@ public class GildedRose {
         item.quality = item.quality - item.quality;
       }
     } else {
-      if (!item.name.equals("Aged Brie")) {
+      if (item.name.equals("Aged Brie")) {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+
+        }
+
+        item.sellIn = item.sellIn - 1;
+
+        if (item.sellIn < 0) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        }
+      } else {
         if (item.quality > 0) {
           if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.quality = item.quality - 1;
           }
         }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
 
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+          item.sellIn = item.sellIn - 1;
         }
-      }
 
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        item.sellIn = item.sellIn - 1;
-      }
-
-      if (item.sellIn < 0) {
-        if (!item.name.equals("Aged Brie")) {
+        if (item.sellIn < 0) {
           if (item.quality > 0) {
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
               item.quality = item.quality - 1;
             }
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
           }
         }
       }
